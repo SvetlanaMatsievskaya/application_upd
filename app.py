1a3e929059efea0c2776fada50ae49f972b5cd7d
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-12-21 02:26:48 +0300

    initial commit

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ def predict():
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
-    '''
-    For direct API calls trought request
-    '''
+
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
