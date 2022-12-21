78c4a856ea7b1afd9a47441c865449c5e820d279
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-12-21 08:15:59 +0300

    change4

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
 def predict():
 
 
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
